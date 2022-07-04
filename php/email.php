1f5444b0546029e24f0fe58729054bfcc689d646
Weslei Igor <wesleigor@hotmail.com>
2022-07-04 11:04:52 -0300

    email

--- php/email.php
+++ php/email.php
@@ -1,24 +1,25 @@
 <?php
 
+
+if(isset($_POST['email']) && !empty($_POST['email']));
+
+
 $nome = addslashes ($_POST ['name']);
 $email = addslashes ($_POST['email']);
 $mensagem = addslashes ($_POST['message']);
 
-
-
-
 $to= "wesleigor@hotmail.com" ;
-$subject= "Formulário do site";
-$body = "Nome: " . $nome . "\n".
-        "Email: " . $email . "\n".
-        "Mensagem: " . $mensagem . "\r\n".
+$subject= "Formulário - Westi.tech";
+$body = "Nome: " . $nome . "\r\n".
+        "Email: " . $email . "\r\n".
+        "Mensagem: " . $mensagem;
 
 $header ="from:suporte@westi.tech"."\r\n".
         "reply-to".$email ."\r\n".
         "x=mailer:PHP/".phpversion();
 
         if(mail($to, $subject, $body, $header)){
-                header("location:./index.html");
+                header("location:index.html");
         
                   }
         else("Não foi possível enviar o e-mail");
